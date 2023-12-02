2cca13fd9c8206992b368870519be7c8aaf172ce
benyltam <ben@welkin.com.hk>
2023-12-02 11:39:05 +0800

    Add files via upload

---
+++ ControlLibrary/Xcheckbox.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ControlLibrary
+{
+    partial class Xcheckbox
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Xcheckbox));
+            this.CheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // CheckBox
+            // 
+            this.CheckBox.AutoSize = true;
+            this.CheckBox.Location = new System.Drawing.Point(0, 0);
+            this.CheckBox.Name = "CheckBox";
+            this.CheckBox.Size = new System.Drawing.Size(12783, 17);
+            this.CheckBox.TabIndex = 0;
+            this.CheckBox.Text = resources.GetString("CheckBox.Text");
+            this.CheckBox.UseVisualStyleBackColor = true;
+            // 
+            // Xcheckbox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.CheckBox);
+            this.Name = "Xcheckbox";
+            this.Size = new System.Drawing.Size(85, 15);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox CheckBox;
+    }
+}
